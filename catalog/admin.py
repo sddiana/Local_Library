de93420e4d0da109413c9c37bb5eebb84436985e
sddiana <mosethvatit@gmail.com>
2025-11-11 19:46:35 +0700

    The admin panel has been completed

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+#inline class
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 1
+    fields = ('title', 'isbn')
+    show_change_link = True
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 1
+    fields = ('status', 'due_back', 'imprint')
+    show_change_link = True
+
+# admin class
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id', 'imprint')
+    list_filter = ('status', 'due_back') 
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+#регистрация моделей
+
+admin.site.register(Genre)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
