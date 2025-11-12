3e07f027494e81bf7cfa0c4b84f2f97523b75d6a
sddiana <mosethvatit@gmail.com>
2025-11-12 13:59:14 +0700

    BookList and DetailList

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
-
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('books/', views.book_list, name='books'),
+    re_path(r'^book/(?P<pk>\d+)/$', views.book_detail, name='book-detail'),
+    path('authors/', views.author_list, name='authors'),
+    path('author/<int:pk>/', views.author_detail, name='author-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Author, BookInstance, Genre  # ← ДОБАВЬТЕ ИМПОРТ!
 
-# Create your views here.
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    keyword = 'Маленькие'
+    num_books_with_keyword = Book.objects.filter(title__icontains=keyword).count()
+
+    keyword_genres = 'Novels'
+    num_genres_with_keyword = Genre.objects.filter(name__icontains=keyword_genres).count()
+
+    return render(
+        request,
+        'catalog/index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_books_with_keyword': num_books_with_keyword,
+            'keyword': keyword,
+            'num_genres_with_keyword': num_genres_with_keyword,
+            'keyword_genres': keyword_genres,
+        },
+    )
+
+def book_list(request):
+    books = Book.objects.all().order_by('title')
+    return render(request, 'catalog/book_list.html', {
+        'book_list': books
+    })
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'catalog/book_detail.html', {
+        'book': book
+    })
+
+def author_list(request):
+    authors = Author.objects.all().order_by('last_name', 'first_name')
+    return render(request, 'catalog/author_list.html', {
+        'author_list': authors
+    })
+
+def author_detail(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    return render(request, 'catalog/author_detail.html', {
+        'author': author
+    })
